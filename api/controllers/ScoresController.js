0e5dd65ab4553c8d086c3e1c530f66adbe200df6
Jason Poon <git@jasonpoon.ca>
2016-08-01 15:57:30 -0700

    update controller

--- api/controllers/ScoresController.js
+++ api/controllers/ScoresController.js
@@ -8,11 +8,15 @@
 
 module.exports = {
   index: function(req, res) {
-    Scores.query('SELECT TOP 5 username, score, RANK() OVER (ORDER BY score DESC) AS rank FROM scores', function(err, scores) {
-    console.log(JSON.stringify(scores));
-      res.send({
-        leaderboard: scores 
-      });
+    Scores.query('SELECT TOP 5 *, RANK() OVER (ORDER BY score DESC) AS rank FROM scores', function(err, scores) {
+      res.send(scores);
+    });
+  },
+
+  findOne: function(req, res) {
+    var id = req.param('id');
+    Scores.query('SELECT * FROM (SELECT *, RANK() OVER (ORDER BY score DESC) AS rank FROM scores) ranks WHERE ranks.id = ' + id, function(err, score) {
+      res.send(score);
     });
   }
 };
